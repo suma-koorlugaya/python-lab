24a3ed766b08e540003191db325ba0432299df7e
suma-koorlugaya <116092187+suma-koorlugaya@users.noreply.github.com>
2022-12-13 21:41:58 +0530

    Update 56.py

--- 56.py
+++ 56.py
@@ -1,8 +1,8 @@
 import re
 p=re.compile('\d{3}[-|\.|\s]\d{3}[-|\.|\s]\d{4}')
-r=p.search('345-766-9087 is an American number')
-print(r.group())
+r=p.findall('3345-766-907 ,886-245-9772')
+print(r)
 
 a=re.compile('\w+\d+\w*@gmail\.com')
-t=a.search('mail id is sumahsn2002@gmail.com')
-print(t.group())
+t=a.findall(' sumahsn2002@gmail.com,shhdh.in,sushma2008@gmail.com')
+print(t)
